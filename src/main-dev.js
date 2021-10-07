278bce5553b9f140d7b5d8d6d8d2ae56c5173c49
xihusiyu <2292656153@qq.com>
2021-10-07 22:12:41 +0800

    完成dist打包

--- src/main-dev.js
+++ src/main-dev.js
@@ -13,11 +13,24 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
+// 导入NProgress 包对应的JS和CSS
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 // 配置请求的路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+
+// 在request拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
   // console.log(config)
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+// 在response拦截器中, 隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Rules from '../components/power/Rules.vue'
-import Cate from '../components/goods/Cate.vue'
-import Params from '../components/goods/Params.vue'
-import GoodsList from '../components/goods/List.vue'
-import Add from '../components/goods/Add.vue'
-import Order from '../components/order/Order.vue'
-import Report from '../components/report/Report.vue'
+
+// import Login from '../components/Login.vue'
+const Login = () => import(/* webpackChunckName: "login_home_welcome" */ '../components/Login.vue')
+
+// import Home from '../components/Home.vue'
+const Home = () => import(/* webpackChunckName: "login_home_welcome" */ '../components/Home.vue')
+// import Welcome from '../components/Welcome.vue'
+const Welcome = () => import(/* webpackChunckName: "login_home_welcome" */ '../components/Welcome.vue')
+
+// import Users from '../components/user/Users.vue'
+const Users = () => import(/* webpackChunckName: "Users_Rights_Roles" */ '../components/user/Users.vue')
+// import Rights from '../components/power/Rights.vue'
+const Rights = () => import(/* webpackChunckName: "Users_Rights_Roles" */ '../components/power/Rights.vue')
+// import Rules from '../components/power/Rules.vue'
+const Roles = () => import(/* webpackChunckName: "Users_Rights_Roles" */ '../components/power/Roles.vue')
+
+// import Cate from '../components/goods/Cate.vue'
+const Cate = () => import(/* webpackChunckName: "Cate_Params" */ '../components/goods/Cate.vue')
+// import Params from '../components/goods/Params.vue'
+const Params = () => import(/* webpackChunckName: "Cate_Params" */ '../components/goods/Params.vue')
+
+// import GoodsList from '../components/goods/List.vue'
+const GoodsList = () => import(/* webpackChunckName: "GoodsList_Add" */ '../components/goods/List.vue')
+// import Add from '../components/goods/Add.vue'
+const Add = () => import(/* webpackChunckName: "GoodsList_Add" */ '../components/goods/Add.vue')
+
+// import Order from '../components/order/Order.vue'
+const Order = () => import(/* webpackChunckName: "Order_Report" */ '../components/order/Order.vue')
+// import Report from '../components/report/Report.vue'
+const Report = () => import(/* webpackChunckName: "Order_Report" */ '../components/report/Report.vue')
 
 Vue.use(Router)
 const routes = [
@@ -31,7 +49,7 @@ const routes = [
       { path: '/welcome', component: Welcome },
       { path: '/users', component: Users },
       { path: '/rights', component: Rights },
-      { path: '/roles', component: Rules },
+      { path: '/roles', component: Roles },
       { path: '/categories', component: Cate },
       { path: '/params', component: Params },
       { path: '/goods', component: GoodsList },
